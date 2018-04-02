134bbe5d5e4a9bcb34e7b1612095b243419e2323
Sachin Mittal <sachin.narenmittal@gmail.com>
2018-04-02 18:34:26 +0530

    Use case of Internal visibility

---
+++ Solidity/internal.sol
@@ -0,0 +1,17 @@
+contract Sandwich {
+  uint private sandwichesEaten = 0;
+
+  function eat() internal {
+    sandwichesEaten++;
+  }
+}
+
+contract BLT is Sandwich {
+  uint private baconSandwichesEaten = 0;
+
+  function eatWithBacon() public returns (string) {
+    baconSandwichesEaten++;
+    // We can call this here because it's internal
+    eat();
+  }
+}
