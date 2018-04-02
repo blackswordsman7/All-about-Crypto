7cf5e9f7a0bf3f159b27dd53475b2d3c016d5df0
Sachin Mittal <sachin.narenmittal@gmail.com>
2018-04-02 17:10:15 +0530

    Create exampleMapping.sol

---
+++ Solidity/exampleMapping.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.19;
+
+contract exampleMapping{
+    
+
+mapping (address => uint) favoriteNumber;
+
+function setMyNumber(uint _myNumber) public {
+  // Update our `favoriteNumber` mapping to store `_myNumber` under `msg.sender`
+  favoriteNumber[msg.sender] = _myNumber;
+  // ^ The syntax for storing data in a mapping is just like with arrays
+}
+
+function whatIsMyNumber() public view returns (uint) {
+  // Retrieve the value stored in the sender's address
+  // Will be `0` if the sender hasn't called `setMyNumber` yet
+  return favoriteNumber[msg.sender];
+}
+}
