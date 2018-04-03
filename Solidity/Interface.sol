72e6b0aa382a2637a8856e29eb24cf75a0746fc5
Sachin Mittal <sachin.narenmittal@gmail.com>
2018-04-03 18:22:49 +0530

    Use case of Interface

---
+++ Solidity/Interface.sol
@@ -0,0 +1,29 @@
+contract LuckyNumber {
+  mapping(address => uint) numbers;
+
+  function setNum(uint _num) public {
+    numbers[msg.sender] = _num;
+  }
+
+  function getNum(address _myAddress) public view returns (uint) {
+    return numbers[_myAddress];
+  }
+}
+
+contract NumberInterface {
+  function getNum(address _myAddress) public view returns (uint);
+}
+
+contract MyContract {
+  address NumberInterfaceAddress = 0xab38... 
+  // ^ The address of the FavoriteNumber contract on Ethereum
+  
+  NumberInterface numberContract = NumberInterface(NumberInterfaceAddress);
+  // Now `numberContract` is pointing to the other contract
+
+  function someFunction() public {
+    // Now we can call `getNum` from that contract:
+    uint num = numberContract.getNum(msg.sender);
+    // ...and do something with `num` here
+  }
+}
